492a651742a79eaaa2c02e76171392ffd710b282
Mona Zhao <monazhao59@gmail.com>
2026-04-01 00:48:14 -0700

    change to empty template

--- hw0/hw0.c
+++ hw0/hw0.c
@@ -1,25 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "stack.h"
+#include <stdio.h>
 
 int main(void) {
-    Stack s;
-    FILE *fp;
-
-    initStack(&s);
-
-    push(&s, 10);
-    push(&s, 20);
-
-    fp = fopen("output", "w");
-    if (fp == NULL) {
-        fprintf(stderr, "Failed to open output file\n");
-        return 1;
-    }
-
-    fprintf(fp, "Top element: %d\n", peek(&s));
-    fprintf(fp, "Popped: %d\n", pop(&s));
-
-    fclose(fp);
+    // TODO: implement
+    printf("Hello HW0\n");
     return 0;
 }
